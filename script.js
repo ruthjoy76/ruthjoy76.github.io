104fd7a817d6b6003fe081243f53669825d64d74
ruthjoy76 <ruthjoy76@gmail.com>
2023-03-26 01:31:34 +0800

    Update Portfolio

---
+++ script.js
@@ -0,0 +1,15 @@
+const mobileNav = document.querySelector('.mobile-nav')
+    const menuBtn = document.querySelector('.menu-btn')
+    const navlinks = document.querySelector('.nav-links')
+    const closeBtn = document.querySelector('.close-btn')
+
+    mobileNav.addEventListener('click',(e)=>{
+        navlinks.classList.toggle('mobile-menu')
+        if(e.target === menuBtn){
+            menuBtn.replaceWith(closeBtn)
+            closeBtn.style.display = 'block'
+        }
+        if(e.target === closeBtn){
+            closeBtn.replaceWith(menuBtn)
+        }
+    })
